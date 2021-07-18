7666365deaddb586df2ac4f01aa59045c0f6c532
Casey Scarborough <casey.scarborough@nscorp.com>
2021-07-18 16:34:29 -0400

    Cleanup brew audit and style errors

--- Formula/subtitler.rb
+++ Formula/subtitler.rb
@@ -1,15 +1,15 @@
 class Subtitler < Formula
-  desc "A cross-platform, full-featured Java library and CLI utility for working with subtitles."
+  desc "Cross-platform Java library and CLI utility for working with subtitles"
   homepage "https://github.com/caseyscarborough/subtitler"
   url "https://github.com/caseyscarborough/subtitler/releases/download/1.0.3/subtitler.zip"
   sha256 "052a3f9fc59e586ddd3bf1b532d7f7cf5d05b8c3bc7a66c6479abccdddd65daa"
   depends_on "openjdk@11"
 
   def install
-  	rm Dir["bin/subtitler.bat"]
-  	chmod 0755, "bin/subtitler"
+    rm "bin/subtitler.bat"
+    chmod 0755, "bin/subtitler"
     bin.install "bin/subtitler"
-    (libexec/"lib").install Dir["lib/*"]	
+    (libexec/"lib").install Dir["lib/*"]
 
     bin.env_script_all_files(libexec/"bin", JAVA_HOME: Formula["openjdk@11"].opt_prefix)
   end
