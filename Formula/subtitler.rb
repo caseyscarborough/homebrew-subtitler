7133c6fded4b8c8838bfdb3cb40b51636bfb0781
Casey Scarborough <casey.scarborough@nscorp.com>
2021-07-18 15:13:15 -0400

    Initial commit

---
+++ Formula/subtitler.rb
@@ -0,0 +1,15 @@
+class Subtitler < Formula
+  desc "A cross-platform, full-featured Java library and CLI utility for working with subtitles."
+  homepage "https://github.com/caseyscarborough/subtitler"
+  url "https://github.com/caseyscarborough/subtitler/releases/download/1.0.2/subtitler.zip"
+  sha256 "23bf671ca253d4f0bb99e0954c71473a7c391fb2253ecf0adc72ad7e798475a9"
+  version "1.0.2"
+  depends_on :java => "1.8"
+
+  def install
+  	rm Dir["bin/subtitler.bat"]
+
+  	chmod 0755, "bin/subtitler"
+    bin.install "bin/subtitler"
+  end
+end
---
+++ README.md
@@ -0,0 +1,3 @@
+# homebrew-subtitler
+
+This is the [Homebrew](https://brew.sh/) tap for the [subtitler](https://github.com/caseyscarborough/subtitler) repo.
